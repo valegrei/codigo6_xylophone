7ee643c2de92b9634f789a73039cddb306fd35af
valegrei <victoralegre@uni.pe>
2023-02-10 17:57:07 -0500

    First Commi

---
+++ android/app/src/main/kotlin/com/example/codigo6_xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codigo6_xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:codigo6_xylophone/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
