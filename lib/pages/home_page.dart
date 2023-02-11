ee538c85445944ef1287ae6ef6b02bfe4c49fd58
valegrei <victoralegre@uni.pe>
2023-02-10 21:51:02 -0500

    Notas

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,90 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  AudioPlayer mandarina = AudioPlayer();
+
+  void playSound(int nro) {
+    mandarina.play(AssetSource("audios/note$nro.wav"));
+  }
+
+  Expanded buildKey(Color color, int nro) {
+    return Expanded(
+        child: MaterialButton(
+            color: color,
+            onPressed: () {
+              playSound(nro);
+            }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // ElevatedButton(
+          //     onPressed: (){
+          //       AudioPlayer mandarina = AudioPlayer();
+          //       mandarina.play(AssetSource("audios/note1.wav"));
+          //     },
+          //     child: const Text("Click")
+          // ),
+          buildKey(Colors.red, 1),
+          buildKey(Colors.orange, 2),
+          buildKey(Colors.yellow, 3),
+          buildKey(Colors.green, 4),
+          buildKey(Colors.blue, 5),
+          buildKey(Colors.indigo, 6),
+          buildKey(Colors.purple, 7),
+          // Expanded(
+          //     child: MaterialButton(
+          //         color: Colors.red,
+          //         onPressed: () {
+          //           playSound(1);
+          //         }
+          //     )
+          // ),
+          // Expanded(
+          //     child: MaterialButton(
+          //         color: Colors.blue,
+          //         onPressed: () {
+          //           playSound(2);
+          //         }
+          //     )
+          // ),
+          // Expanded(
+          //     child: MaterialButton(
+          //         color: Colors.yellow,
+          //         onPressed: () {
+          //           playSound(3);
+          //         }
+          //     )
+          // ),
+          // Expanded(
+          //   flex: 5,
+          //   child: Container(
+          //     color: Colors.red,
+          //   ),
+          // ),
+          // Expanded(
+          //   flex: 1,
+          //   child: Container(
+          //     color: Colors.yellow,
+          //   ),
+          // ),
+          // Expanded(
+          //   flex: 1,
+          //   child: Container(
+          //     color: Colors.blue,
+          //   ),
+          // ),
+          // Image.network(
+          //   "https://images.pexels.com/photos/14967402/pexels-photo-14967402.jpeg"
+          // ),
+        ],
+      ),
+    );
   }
 }
